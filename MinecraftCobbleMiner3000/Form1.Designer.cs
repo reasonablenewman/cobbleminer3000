21d40e2add9b041e74388224df559836e490c7d3
William Sandbrink <wilsandbrink@gmail.com>
2019-02-03 01:36:36 +0100

    Added cobbleminer3000 project

---
+++ MinecraftCobbleMiner3000/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace MinecraftCobbleMiner3000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bDelay = new System.Windows.Forms.NumericUpDown();
+            this.rDelay = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bDelay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rDelay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bDelay
+            // 
+            this.bDelay.DecimalPlaces = 1;
+            this.bDelay.Location = new System.Drawing.Point(12, 25);
+            this.bDelay.Name = "bDelay";
+            this.bDelay.Size = new System.Drawing.Size(120, 20);
+            this.bDelay.TabIndex = 0;
+            this.bDelay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // rDelay
+            // 
+            this.rDelay.Location = new System.Drawing.Point(12, 51);
+            this.rDelay.Name = "rDelay";
+            this.rDelay.Size = new System.Drawing.Size(120, 20);
+            this.rDelay.TabIndex = 1;
+            this.rDelay.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Block mine delay";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Block respawn delay";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "mine";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 116);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(272, 251);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "USE WITH CAUTION!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 379);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rDelay);
+            this.Controls.Add(this.bDelay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CobbleMiner3000";
+            ((System.ComponentModel.ISupportInitialize)(this.bDelay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rDelay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown bDelay;
+        private System.Windows.Forms.NumericUpDown rDelay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MinecraftCobbleMiner3000/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinecraftCobbleMiner3000
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void mine(decimal bdelay, decimal wdelay)
+        {
+            // Convert to ms
+            bdelay = bdelay * 1000;
+            wdelay = wdelay * 1000;
+
+            // Convert decimal to integers
+            int icbdelay = Convert.ToInt32(Math.Round(bdelay, 0));
+            int icwdelay = Convert.ToInt32(Math.Round(wdelay, 0));
+
+            // Wait a bit before mining
+            await Task.Delay(icwdelay);
+
+            outputPrint("Beginning mining operation.");
+
+            while (checkBox1.Checked)
+            {
+                int X = Cursor.Position.X;
+                int Y = Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, 0, 0);
+                outputPrint("Left mouse down");
+                await Task.Delay(icbdelay);
+                mouse_event(MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                outputPrint("Left mouse up");
+                await Task.Delay(icwdelay);
+            }
+            outputPrint("Ending mining operation.");
+        }
+
+        private void outputPrint(string msg)
+        {
+            msg += "\n";
+            richTextBox1.AppendText(msg);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                mine(bDelay.Value, rDelay.Value);
+
+            }
+        }
+    }
+}
